f97931a50d59360d1dc1a5a8fe79be47d3a55d53
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-09 20:59:28 -0400

    Modified index.js model

--- models/index.js
+++ models/index.js
@@ -1,13 +1,38 @@
-const User = require('./User');
-const Project = require('./Project');
+// INDEX MODEL
+// ---------------------------------------------------------------------------
 
-User.hasMany(Project, {
+const Comments = require('./Comments');
+const Posts = require('./Posts');
+const Users = require('./Users');
+
+Comments.belongsTo(Posts, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+Comments.belongsTo(Users, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Posts.belongsTo(Users, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Posts.hasMany(Comments, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+Users.hasMany(Posts, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+Users.hasMany(Comments, {
+  foreignKey:'user_id',
+  onDelete: 'CASCADE',
 });
 
-module.exports = { User, Project };
+module.exports = { Comments, Posts, Users};
