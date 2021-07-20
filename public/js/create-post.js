8c160020fed750ac1bfcf87477fa889b104fcb4c
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-20 18:07:51 -0400

    Changed information in js/create-post.js

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -5,13 +5,13 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    const posts_content = document.querySelector('input[name="post-content"]').value;
+    const post_content = document.querySelector('input[name="post-content"]').value;
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
             title,
-            posts_content
+            post_content
         }),
         headers: {
             'Content-Type': 'application/json'
