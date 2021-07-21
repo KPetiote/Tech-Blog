941eff78b82cb0be11c2f7df1a631c67379fc772
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-21 15:38:50 -0400

    fixed several issues with seeds

--- seeds/posts-data.js
+++ seeds/posts-data.js
@@ -6,25 +6,20 @@ const { Post } = require('../models');
 const seedPosts = 
 [
   {
-    title: "First Post",
-    post_content: "This is my first post ",
-    user_id: 3
+    "title": "First Post",
+    "post_content": "This is my first post ",
+    "user_id": 3
   },
   {
-    title: "Second Post",
-    post_content: "This is my second post ",
-    user_id: 1
+    "title": "Second Post",
+    "post_content": "This is my second post ",
+    "user_id": 1
   },
   {
-    title: "Third Post",
-    post_content: "This is my third post ",
-    user_id: 2
+    "title": "Third Post",
+    "post_content": "This is my third post ",
+    "user_id": 2
   },
-  {
-    title: "Fourth Post",
-    post_content: "This is my fourth post ",
-    user_id: 4
-  }
 ]
 const postsData = () => Post.bulkCreate(seedPosts);
 
