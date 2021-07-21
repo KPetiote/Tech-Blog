0b2af7642054aa5bbe73e133d7b6fb8a47b6ef2b
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-21 14:30:36 -0400

    Made Changes in seeds/seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,25 @@
 // SEED JS
 // ---------------------------------------------------------------------------
 
-
-// const { Comments, Users, Posts  } = require('../models');
-
 const usersData = require('./users-data');
-const commentsData = require('./comments-data');
 const postsData = require('./posts-data');
+const commentsData = require('./comments-data');
 
 const sequelize = require('../config/connection');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
 
-  await commentsData();
+  await sequelize.sync({ force: true });
+    console.log('\n----- Database Synced Successfully -----\n');
 
   await usersData();
+    console.log('\n----- Users Seeded Successfully -----\n');
 
   await postsData();
+    console.log('\n----- Posts Seeded Successfully -----\n');
+
+  await commentsData();
+    console.log('\n----- Comments Seeded Successfully -----\n');
 
   process.exit(0);
 
