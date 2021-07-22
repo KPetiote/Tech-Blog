f57fafbb48f4569738fc75857382b9709369ce93
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-21 21:17:43 -0400

    Made several changes to Layout

---
+++ seeds/postsData.js
@@ -0,0 +1,36 @@
+// POSTS DATA JS
+// ---------------------------------------------------------------------------
+
+const { Posts } = require('../models');
+
+const seedPosts = [
+  {
+    "title": "First Post",
+    "posts_content": "This is my first post ",
+    "users_id": 3
+  },
+  {
+    "title": "Second Post",
+    "posts_content": "This is my second post ",
+    "users_id": 1
+  },
+  {
+    "title": "Third Post",
+    "posts_content": "This is my third post ",
+    "users_id": 2
+
+  },
+  {
+    "title": "Fourth Post",
+    "posts_content": "This is my fourth post ",
+    "users_id": 4
+  },
+  {
+    "title": "Fifth Post",
+    "posts_content": "This is my fifth post ",
+    "users_id": 4
+  }
+]
+const postsData = () => Posts.bulkCreate(seedPosts);
+
+module.exports = postsData;
